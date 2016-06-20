5673cd94ea75a022dadca35afc70e23ffda1c838
Alessandro Marin <alessandro@studocu.com>
2016-06-20 11:24:08 +0200

    takes care of server side case where global is defined but not window

--- src/SamuelNorbury/LaravelJsLocalization/Generators/Templates/langjs_with_messages.js
+++ src/SamuelNorbury/LaravelJsLocalization/Generators/Templates/langjs_with_messages.js
@@ -1,1 +1,6 @@
-window.__MESSAGES__ = '{ messages }';
+var messages = '{ messages }';
+if (typeof window !== 'undefined') {
+    window.__MESSAGES__ = messages;
+} else if (typeof GLOBAL !== 'undefined') {
+    GLOBAL.__MESSAGES__ = messages;
+}
