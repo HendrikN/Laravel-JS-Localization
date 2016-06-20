6abab85a65f1a40e26df6f461dfc53d33738235f
Alessandro Marin <alessandro@studocu.com>
2016-06-20 11:36:33 +0200

    uses module istead of gllobal

--- src/SamuelNorbury/LaravelJsLocalization/Generators/Templates/langjs_with_messages.js
+++ src/SamuelNorbury/LaravelJsLocalization/Generators/Templates/langjs_with_messages.js
@@ -1,6 +1,6 @@
 var messages = '{ messages }';
 if (typeof window !== 'undefined') {
     window.__MESSAGES__ = messages;
-} else if (typeof GLOBAL !== 'undefined') {
-    GLOBAL.__MESSAGES__ = messages;
+} else if (typeof module !== 'undefined') {
+    module.exports = messages;
 }
