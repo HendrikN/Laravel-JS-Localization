8d43a39e3499394c1dd3723b2f28d6ce9ded8d7c
Hendrik Nijmeijer <hnijmeijer@lely.com>
2022-09-05 14:10:00 +0200

    fix command

--- src/HendrikN/LaravelJsLocalization/Commands/LangJsCommand.php
+++ src/HendrikN/LaravelJsLocalization/Commands/LangJsCommand.php
@@ -7,55 +7,19 @@ use Symfony\Component\Console\Input\InputArgument;
 
 class LangJsCommand extends Command
 {
-    /**
-     * The command name.
-     */
     protected $signature = 'lang:js {target?} {--c|--compress}';
 
-    /**
-     * The command description.
-     */
     protected $description = 'Generate JS lang files.';
 
-    /**
-     * The generator instance.
-     */
-    protected $generator;
-
-    /**
-     * Construct a new LangJsCommand.
-     *
-     * @param HendrikN\LaravelJsLocalization\Genrators\LangJsGenerator The generator instance.
-     */
-    public function __construct(LangJsGenerator $generator)
+    public function handle(LangJsGenerator $generator)
     {
-        $this->generator = $generator;
-        parent::__construct();
-    }
-
-    /**
-     * Fire the command.
-     */
-    public function handle()
-    {
-        $target = $this->argument('target');
+        $target = $this->argument('target') ?? public_path();
         $options = ['compress' => $this->option('compress')];
 
-        if ($this->generator->generate($target, $options))
-        {
+        if ($generator->generate($target, $options)) {
             return $this->info("Created: {$target}");
         }
 
         $this->error("Could not create: {$target}");
-    }
-
-    /**
-     * Return the public path of the Laravel application.
-     *
-     * @return string
-     */
-    public function getPublicPath()
-    {
-        return public_path();
     }
 }
